6a9127e27df0176a379776a1c2c956dbf7b7c69c
itsiros <jtsiros93@gmail.com>
2025-05-12 20:32:15 +0200

    R

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import wc
+from stats import ascci_dict, wc
 
 def get_book_text(path:str) -> str:
     with open(path) as str:
@@ -10,7 +10,9 @@ def main():
     book_str = get_book_text("./books/frankenstein.txt")
     num_words = wc(book_str)
     print (f"{num_words} words found in the document")
-
+    result = ascci_dict(book_str)
+    for char, data in result.items():
+        print(f"'{char}': {data}")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -2,4 +2,11 @@ def wc(file:str) -> int:
     words = file.split()
     return len(words)
 
-
+def ascci_dict(file:str) -> dict[str, int]:
+    freq = {}
+    for char in file.lower():
+        if char in freq:
+            freq[char] += 1
+        else:
+            freq[char] = 1
+    return freq
